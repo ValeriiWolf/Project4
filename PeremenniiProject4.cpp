37d66d61d23fb15fb2603e73d9d2e2ae3d3d4413
Valerii <lord_liret@mail.ru>
2021-06-03 01:22:35 +0500

    Добавьте файлы проекта.

---
+++ PeremenniiProject4.cpp
@@ -0,0 +1,4 @@
+int Perenosa = 1;
+int Perenosb = 3;
+int Perenosc = 5;
+int Perenosd = 8;
---
+++ Project4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+extern  int Perenosa;
+extern  int Perenosb;
+extern  int Perenosc;
+extern  int Perenosd;
+
+int main()
+{
+    int a, b, c, d;
+    cout << "Enter an integer a" << endl;
+    cin >> a;
+    cout << "Enter an integer b" << endl;
+    cin >> b;
+    cout << "Enter an integer c" << endl;
+    cin >> c;
+    cout << "Enter an integer d, not equal to 0" << endl;
+    cin >> d;
+    float Schet1;
+    Schet1 = a * (b + static_cast <float>(c) / d);
+    cout << Schet1 << endl;
+
+    int ternar;
+    ternar = a <= 21 ? 21 - a : (a - 21) << 1;
+    cout << ternar << endl;
+
+    const unsigned int SIZE2 = 3;
+    int Array1[SIZE2][SIZE2][SIZE2] = {};
+    int pArr;
+    pArr = & Array1[1][1][1];
+    //pArr = &Array1[0][0][0];
+    //pArr[1][1][1] = 10;
+    //cout << *pArr[1][1][1] << endl; 
+    //cout << Array1[1][1][1] << endl;
+    
+    //int *pArr;//= nullptr;
+
+   
+    float Schet2;
+    Schet2 = Perenosa * (Perenosb + static_cast <float>(Perenosc) / Perenosd);
+    cout << Schet2 << endl;
+
+    return 0;
+}
